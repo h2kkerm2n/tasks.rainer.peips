2c034f4e0d302676a0c4583857eaa297a315934c
Rainer Peips <rainer.peips>
2021-09-08 15:07:55 +0300

    first commit

---
+++ test1.js
@@ -0,0 +1,11 @@
+let a = 5
+const b = 10
+let c = a+b
+console.log (c);
+
+a = "5"
+d = a+b
+
+console.log (d)
+
+//tulemust muutub 15 pealt 510//
---
+++ test2.js
@@ -0,0 +1,9 @@
+const eesnimi = 'Rainer';
+const sunnipaev = "11";
+const sunnikuu = "8";
+const sunniaasta = "1990";
+const hobid = ['jalgpall','arvutimangud'];
+const object ={eesnimi,sunnikuu,sunniaasta,sunniaasta,hobid};
+
+console.log(object);
+console.log(sunnipaev+"."+sunnikuu+"."+sunniaasta)
