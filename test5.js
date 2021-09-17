6f6d70037ffc650e583dc79385f60a905ffb0870
h2kkerm2n <90323783+h2kkerm2n@users.noreply.github.com>
2021-09-17 18:05:37 +0300

    test5.js

--- test5.js
+++ test5.js
@@ -1,4 +1,13 @@
-const fruits = ["Red" , "Green" , "white" , "Black"];
+const colors = ["Red" , "Green" , "white" , "Black"];
+
+const a = colors.toString(",");
+console.log(a)
+//output , //
+
+const b = colors.join(".");
+ console.log (b)
+ //output . // 
  
- 
-console.log (fruits)
+ const c = colors.join("+");
+ console.log(c)
+ //outout + //
