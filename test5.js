8de2cd041276d2c15123967260654a3fd8b6f9d3
h2kkerm2n <90323783+h2kkerm2n@users.noreply.github.com>
2021-09-17 18:13:02 +0300

    test5.js

--- test5.js
+++ test5.js
@@ -4,10 +4,14 @@ const a = colors.toString(",");
 console.log(a)
 //output , //
 
-const b = colors.join(".");
+const b = colors.join(". ");
  console.log (b)
  //output . // 
  
- const c = colors.join("+");
+ const c = colors.join("+ ");
  console.log(c)
  //outout + //
+
+
+
+ cons
