9a2e78efb43715e3af89eea4861dd1c85f6e3617
h2kkerm2n <90323783+h2kkerm2n@users.noreply.github.com>
2021-09-20 21:28:30 +0300

    uuendus

--- test5.js
+++ test5.js
@@ -1,14 +1,17 @@
-const colors = ["Red" , "Green" , "white" , "Black"];
+const mycolors1 = ['Red'+','+'Green'+','+'White'+','+'Black. ' ];
 
-const a = colors.toString(",");
-console.log(a)
-//output , //
+for(let i = 0; i < mycolors1.lenght; i++){
+    console.log(mycolors1[1])
+}
 
-const b = colors.join(". ");
- console.log (b)
- //output . // 
- 
- const c = colors.join("+ ");
- console.log(c)
- //outout + //
+const mycolors2 = ['Red'+','+'Green'+','+'White'+','+'Black. ' ]
 
+for(let i = 0; i < mycolors2.lenght; i++){
+    console.log(mycolors2[2])
+}
+
+const mycolors3 = ['Red'+','+'Green'+','+'White'+','+'Black. ' ]
+
+for(let i = 0; i < mycolors3.lenght; i++){
+    console.log(mycolors3[3])
+}
