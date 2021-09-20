8032aeae20b208647469751eb9fe5755465e8611
h2kkerm2n <90323783+h2kkerm2n@users.noreply.github.com>
2021-09-20 21:36:25 +0300

    ülesanne

---
+++ test5.1.js
@@ -0,0 +1,15 @@
+const mycolors = ["Red", "Green", "White", "Black"];
+const a = mycolors.tostring();
+console.log(a)
+
+//output Red,green,white,black
+
+const c = mycolors.join(", ");
+console.log(c)
+ 
+//output [ 'Red', 'Green', 'White', 'Black' ]//
+
+const b = mycolors.join(" + ");
+console.log(b)
+
+//Output: Red + Green + White + Black//
