3c3be39c7a44c593d98ab12fc55846399f361241
h2kkerm2n <90323783+h2kkerm2n@users.noreply.github.com>
2021-09-27 11:43:37 +0300

    testhello.js

---
+++ test.hello.js
@@ -0,0 +1,8 @@
+function repeatstr(input, times) {
+    let result = input.repeat(times);
+    console.log(result);
+    }
+    repeatstr("Hello!", [1]);
+    repeatstr("Hello!", [2]);
+    repeatstr("Hello!", [3]);
+    repeatstr("Hello!", [12])
